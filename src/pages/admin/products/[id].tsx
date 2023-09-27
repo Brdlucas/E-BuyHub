f1263d65cef216cc72c6b66371ca9ae645b403f3
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2023-09-27 21:41:36 +0200

    Update n°2  27/09/2023

--- src/pages/admin/products/[id].tsx
+++ src/pages/admin/products/[id].tsx
@@ -1,27 +1,26 @@
-import { error } from "console"
 
 export default function UpdateProducts({ props }: any) {
     return (
         <div>
-            <p>{props.name}</p>
+            <p>ss</p>
         </div>
     )
 
 }
 
 
-export async function getStaticProps({ params }: any) {
-    const post = await fetch(`http://localhost:3000/api/products/${params._id}`).then(r => r.json())
-    return { props: JSON.stringify(post.data) }
-}
+// export async function getStaticProps({ params }: any) {
+//     const post = await fetch(`http://localhost:3000/api/products/${params._id}`).then(r => r.json())
+//     return { props: post.data }
+// }
 
 
-export async function getStaticPaths() {
-    const posts = await fetch('http://localhost:3000/api/products').then(r => r.json())
-    return {
-        paths: posts.data.map((props: any) => ({
-            params: { id: props._id.toString() }
-        })),
-        fallback: false,
-    }
-}
+// export async function getStaticPaths() {
+//     const posts = await fetch('http://localhost:3000/api/products').then(r => r.json())
+//     return {
+//         paths: posts.data.map((props: any) => ({
+//             params: { id: props._id.toString() }
+//         })),
+//         fallback: false,
+//     }
+// }
